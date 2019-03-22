22541a1d05fe0cb15a08ba5f827c3eeff9adfd9f
Ebenezer Komla Gavua <gavua@iit.uni-miskolc.hu>
2019-03-22 13:04:35 +0200

    work in progress

--- src/main/java/hu/unimiskolc/iit/distsys/PhMachFill.java
+++ src/main/java/hu/unimiskolc/iit/distsys/PhMachFill.java
@@ -2,10 +2,20 @@ package hu.unimiskolc.iit.distsys;
 
 
 
+import java.util.Collection;
+
+import com.sun.corba.se.spi.activation.Repository;
+
 import hu.mta.sztaki.lpds.cloud.simulator.Timed;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.IaaSService;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VMManager.VMManagementException;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ConstantConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.io.NetworkNode.NetworkException;
+import hu.mta.sztaki.lpds.cloud.simulator.io.StorageObject;
 import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
 import hu.unimiskolc.iit.distsys.interfaces.FillInAllPMs;
 
@@ -13,25 +23,59 @@ import hu.unimiskolc.iit.distsys.interfaces.FillInAllPMs;
 
 public class PhMachFill implements FillInAllPMs {
 
+	ConstantConstraints cc1;
+	ResourceConstraints rc1;
+	VirtualMachine[] vms1;
+
 	@Override
-	public void filler(IaaSService iaas, int vmCount) {
-		// TODO Auto-generated method stub
-		
-		VirtualAppliance va = new VirtualAppliance("va",1,0);
-		
-		for (int i=0;i<10;i++)
-		{
-			try{ 
-				ResourceConstraints asd = iaas.machines.get(i).getCapacities();
-				ResourceConstraints rcc = new AlterableResourceConstraints(asd.getRequiredCPUs(),asd.getRequiredProcessingPower(),asd.getRequiredMemory());
-				iaas.requestVM(va,rcc,iaas.machines.get(i).localDisk,iaas.machines.size());
-			}
-			catch (Exception e){}
+	public void filler1(IaaSService iaas, int vmCount){
+		double maxCPU = 0.0;
+		double maxprocessing = 0.0;
+		long maxmemory = 0;
+		hu.mta.sztaki.lpds.cloud.simulator.io.Repository repo = iaas.repositories.get(0);
+		Collection<StorageObject> sos = iaas.repositories.get(0).contents();
+		StorageObject so = sos.iterator().next();
+		VirtualAppliance va = (VirtualAppliance)so;
+
+		for(PhysicalMachine pm : iaas.machines){
+			System.out.println(pm.freeCapacities.getRequiredCPUs());
 		}
 
-		Timed.simulateUntilLastEvent();
+
+		System.out.println("----------");
+
+		for(int i=0;i<vmCount;i++){
+			maxCPU = 0.0;
+			for(PhysicalMachine pm : iaas.machines){
+				if(pm.freeCapacities.getRequiredCPUs()>maxCPU){
+					maxCPU = pm.freeCapacities.getRequiredCPUs();
+					maxmemory = pm.freeCapacities.getRequiredMemory();
+					maxprocessing = pm.freeCapacities.getRequiredProcessingPower();
+				}
+			}
+
+			System.out.println(maxCPU);
+
+			if(i<vmCount-iaas.machines.size()){
+				cc1 = new ConstantConstraints(maxCPU/10.0, maxprocessing, maxmemory/10);
+			}else{
+				cc1 = new ConstantConstraints(maxCPU, maxprocessing, maxmemory);
+			}
+
+			try{
+				vms1 = iaas.requestVM(va, cc1, repo, 1);
+				System.out.println(vms1[0].getState());
+				Timed.simulateUntilLastEvent();
+				System.out.println(vms1[0].getState());
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
 	}
 
+	@Override
+	public void filler(IaaSService iaas, int vmCount) throws NetworkException {
+		// TODO Auto-generated method stub
+		
 	}
-
-
+}
